3398826559ef5a3dd89e28bb25b4c25afff770be
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-07-02 13:22:23 +0530

    Time: 3 ms (90.95%) | Memory: 43.2 MB (49.41%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,27 +1,28 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-       HashMap<Integer, Integer> map = new HashMap();
+      HashMap<Integer, Integer> map = new HashMap();
 
-        List<Integer> ansList = new ArrayList();
         for (int j : nums1) {
-
-            if (map.containsKey(j)) {
-                map.put(j, map.get(j) + 1);
+            int value=map.getOrDefault(j,-1);
+            if (value!=-1) {
+                map.put(j, value+1);
             } else {
                 map.put(j, 1);
             }
         }
 
+        List<Integer> ansList = new ArrayList();
         for (int j : nums2) {
-            if (map.containsKey(j) && map.get(j) > 0) {
+            int value=map.getOrDefault(j,-1);
+            if (value > 0) {
                 ansList.add(j);
-                map.put(j, map.get(j) - 1);
+                map.put(j, value - 1);
             }
 
         }
-
-        int[] ans = new int[ansList.size()];
-        for (int i = 0; i < ansList.size(); i++) {
+        int ansSize=ansList.size();
+        int[] ans = new int[ansSize];
+        for (int i = 0; i < ansSize; i++) {
             ans[i] = ansList.get(i);
         }
 
