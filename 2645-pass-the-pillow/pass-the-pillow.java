a878b4074359bc3cf8a1d00ba07659fc9c59c7ec
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-07-08 17:07:48 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (95.71%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.java
+++ 2645-pass-the-pillow/pass-the-pillow.java
@@ -6,28 +6,10 @@ class Solution {
             return ((time / (n-1)) % 2) == 0 ? 1 : n;
         }
 
-        int ans = 1;
-
-        boolean increase = true;
-
-        for (int i = 1; i <= time; i++) {
-            if (time <= 0)
-                return ans;
-
-            if (ans == n) {
-                increase = false;
-            }
-
-            if (ans == 1) {
-                increase = true;
-            }
-
-            if (increase)
-                ans++;
-            else
-                ans--;
+        if((time / (n-1)) % 2==0){
+            return 1 + (time % (n-1));
+        }else {
+            return n - (time % (n-1));
         }
-
-        return ans;
     }
 }
