7df6276ff28ea6a4886bc71e898b645829f08ded
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-07-10 17:12:18 +0530

    Time: 1 ms (99.42%) | Memory: 42.6 MB (5.90%) - LeetSync

--- DailyStreak/1720-crawler-log-folder/crawler-log-folder.java
+++ DailyStreak/1720-crawler-log-folder/crawler-log-folder.java
@@ -2,8 +2,11 @@ class Solution {
     public int minOperations(String[] logs) {
         int ans = 0;
 
+        // if it is dir then ans++ else if ../ then ans-- only if ans >0.
         for (String log : logs) {
+            // if first char is not '.' means it is dir!
             if (log.charAt(0)!='.') ans++; 
+            // ../ means moving from sub to parent dir!
             else if (ans > 0 & "../".equals(log))  ans--;
         }
 
