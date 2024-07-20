5c23092174f78a95b7698cfdd53c179275eafa28
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-07-20 18:38:29 +0530

    Time: 2 ms (75.13%) | Memory: 53.4 MB (6.40%) - LeetSync

--- daily-streak/169-majority-element/majority-element.java
+++ daily-streak/169-majority-element/majority-element.java
@@ -1,18 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int len=nums.length;
+        int len = nums.length;
 
-        int majElement=nums[0];
-        int count=0;
-        for(int i=0;i<len;i++){
-            if(nums[i]!=majElement){
-                if(count==0){
-                    majElement=nums[i];
-                    count++;
-                }else{
-                    count--;
-                }
-            }else{
+        int majElement = nums[0];
+        int count = 0;
+        for (int i = 0; i < len; i++) {
+            if (count == 0) {
+                majElement = nums[i];
+                count++;
+            } else if (nums[i] != majElement && count != 0) {
+                count--;
+            } else {
                 count++;
             }
         }
