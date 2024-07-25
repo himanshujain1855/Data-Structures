1a446cbfad1d456e252b72318e378426e312289a
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-07-25 21:50:42 +0530

    Time: 8 ms (50.75%) | Memory: 45.1 MB (78.45%) - LeetSync

--- daily-streak/2502-sort-the-people/sort-the-people.java
+++ daily-streak/2502-sort-the-people/sort-the-people.java
@@ -1,14 +1,21 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        Integer[] idx = new Integer[names.length];
+        Integer[] idx=new Integer[names.length];
 
         for (int i = 0; i < names.length; i++) {
-            idx[i] = i;
+            idx[i]=i;
         }
-        Arrays.sort(idx, (i1, i2) -> Integer.compare(heights[i2],heights[i1]));
-        String[] output = new String[names.length];
+        Arrays.sort(idx, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer i1, Integer i2) {
+                if(heights[i1] < heights[i2]) return 1;
+                else if (heights[i1] > heights[i2]) return -1;
+                return 0;
+            }
+        });
+        String[] output=new String[names.length];
         for (int i = 0; i < names.length; i++) {
-            output[i] = names[idx[i]];
+            output[i]=names[idx[i]];
         }
         return output;
     }
