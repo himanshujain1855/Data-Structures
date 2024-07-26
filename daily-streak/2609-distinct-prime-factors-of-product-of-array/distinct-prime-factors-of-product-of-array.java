07a30672a0f54e6165817d883b2d53a58a9d40e1
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-07-26 13:36:25 +0530

    Time: 10 ms (88.85%) | Memory: 45.1 MB (18.58%) - LeetSync

--- daily-streak/2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.java
+++ daily-streak/2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.java
@@ -8,29 +8,59 @@ class Solution {
 
             int n = nums[i];
 
+            boolean nBy2 = false;
+            boolean nBy3 = false;
+            boolean nBy5 = false;
+
             while (n % 2 == 0) {
                 n = n / 2;
+                nBy2 = true;
+            }
+
+            if (nBy2) {
                 primeFac.add(2);
             }
 
             while (n % 3 == 0) {
                 n = n / 3;
+                nBy3 = true;
+            }
+
+            if (nBy3) {
                 primeFac.add(3);
             }
 
             while (n % 5 == 0) {
                 n = n / 5;
+                nBy5 = true;
+            }
+
+            if (nBy5) {
                 primeFac.add(5);
             }
+
+            boolean nByj = false;
+
+            boolean nByjPlus4 = false;
 
             for (int j = 7; j * j <= n; j += 6) {
                 while (n % j == 0) {
                     n /= j;
+                    nByj = true;
+
+                }
+
+                if (nByj) {
                     primeFac.add(j);
                 }
 
                 while (n % (j + 4) == 0) {
                     n /= (j + 4);
+                    nByjPlus4 = true;
+
+                }
+
+                if (nByjPlus4) {
                     primeFac.add(j + 4);
                 }
             }
