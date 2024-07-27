73d3a1bc2a6e60687bbee3febdcb1aa7159cef99
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-07-27 15:17:40 +0530

    Time: 2 ms (93.30%) | Memory: 46.5 MB (59.27%) - LeetSync

--- daily-streak/338-counting-bits/counting-bits.java
+++ daily-streak/338-counting-bits/counting-bits.java
@@ -13,8 +13,8 @@ class Solution {
         int count=0;
 
         while(n>0){
-            if((n & 1) == 1) count++;
-            n=n/2;
+            n&=n-1;
+            count++;
         }
 
         return count;
