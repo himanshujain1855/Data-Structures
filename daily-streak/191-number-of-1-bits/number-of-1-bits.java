af08dba6b40dabce65efd75fc4dbbcb633d99cec
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-07-28 12:41:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (16.87%) - LeetSync

--- daily-streak/191-number-of-1-bits/number-of-1-bits.java
+++ daily-streak/191-number-of-1-bits/number-of-1-bits.java
@@ -1,10 +1,16 @@
 class Solution {
-    public int hammingWeight(int n) {
-        int count=0;
-        while(n>0){
-            n = (n & (n-1));
-            count++;
+    static int[] setBitsZeroTo255 = new int[256];
+
+    static {
+        for (int i = 1; i < 256; i++) {
+            setBitsZeroTo255[i] = setBitsZeroTo255[(i & (i - 1))] + 1;
         }
-        return count;
+    }
+
+    public int hammingWeight(int n) {
+        return setBitsZeroTo255[n & 255] +
+                setBitsZeroTo255[((n >> 8) & 255)] +
+                setBitsZeroTo255[((n >> 16) & 255)] +
+                setBitsZeroTo255[(n >> 24)];
     }
 }
