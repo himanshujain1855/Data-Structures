84ccc45982e746691ff5c50c2dce1dc53a795fdc
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-07-28 21:13:54 +0530

    Time: 3 ms (66.93%) | Memory: 45 MB (83.23%) - LeetSync

--- daily-streak/137-single-number-ii/single-number-ii.java
+++ daily-streak/137-single-number-ii/single-number-ii.java
@@ -3,28 +3,15 @@ class Solution {
         int ans=0;
 
         for(int i=0;i<32;i++){
-            int zeros=0;
-            
             int ones=0;
-
-
+            int mask= (1 << i);
             for(int num:nums){
-                int mask= (1 << i);
-
                 if((num & mask)!=0){
                     ones++;
-                }else{
-                    zeros++;
                 }
             }
-
-            zeros=zeros % 3;
-            ones=ones % 3;
-
             if((ones % 3) > 0){
                 ans = (ans | (1 << i)); 
-            }else{
-                ans = ((ans & ~(1 << i))); 
             }
         }
 
