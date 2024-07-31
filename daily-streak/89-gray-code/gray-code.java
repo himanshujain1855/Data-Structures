d9bea0a81d202f1e2773e8c315491577436f6f47
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-07-31 18:47:42 +0530

    Time: 2 ms (99.18%) | Memory: 50.7 MB (92.05%) - LeetSync

--- daily-streak/89-gray-code/gray-code.java
+++ daily-streak/89-gray-code/gray-code.java
@@ -1,14 +1,14 @@
 class Solution {
     public List<Integer> grayCode(int n) {
-        int ansSize=(1 << (n));
-        
-        List<Integer> ans=new ArrayList();
-        
-        for(int i=0;i<ansSize;i++){
-            int grayEq = (i ^ (i >> 1));
-            ans.add(grayEq);
+       int ansSize=(1 << (n));
+
+        Integer[] ansArr=new Integer[ansSize];
+
+
+        for(int i=0; i< ansSize;i++){
+            ansArr[i]=(i ^ (i >> 1));
         }
 
-        return ans;
+        return Arrays.asList(ansArr);
     }
 }
