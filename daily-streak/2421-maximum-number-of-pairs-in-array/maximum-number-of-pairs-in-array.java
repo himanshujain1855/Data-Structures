9d4c5860f386cacc16b330423c263f913a4a5309
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-07-31 20:48:56 +0530

    Time: 1 ms (79.39%) | Memory: 41.9 MB (44.29%) - LeetSync

--- daily-streak/2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.java
+++ daily-streak/2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.java
@@ -2,18 +2,15 @@ class Solution {
     public int[] numberOfPairs(int[] nums) {
         int len = nums.length;
 
-        if (len == 1)
-            return new int[] { 0, 1 };
-
         Arrays.sort(nums);
         int pairs = 0;
-        int leftElement = 0;
-
-        for (int i = 1; i < len; i++) {
+        int i=1;
+        while(i<len){
             if (nums[i] == nums[i - 1]) {
                 pairs++;
                 i++;
             }
+            i++;
         }
 
         return new int[] { pairs, len - (pairs*2) };
