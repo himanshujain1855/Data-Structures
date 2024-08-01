df1747fa32dbfb521f5f04f5fa3a7225f7030396
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-08-01 12:29:30 +0530

    Time: 2 ms (49.81%) | Memory: 42 MB (78.01%) - LeetSync

--- daily-streak/67-add-binary/add-binary.java
+++ daily-streak/67-add-binary/add-binary.java
@@ -12,7 +12,7 @@ class Solution {
             char cA = (iA >= 0) ? a.charAt(iA--) : '0';
             char cB = (iB >= 0) ? b.charAt(iB--) : '0';
             sum = sum + cA - 48 + cB - 48;
-            carry = sum >= 2 ? 1 : 0;
+            carry = sum / 2;
             ansSb.append(sum % 2);
         }
        return ansSb.reverse().toString();
