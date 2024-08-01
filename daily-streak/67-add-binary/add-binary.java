ff94868cf4940cda71d90421ec8cd13fef4f4e3c
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-08-01 12:27:16 +0530

    Time: 2 ms (49.81%) | Memory: 42 MB (78.01%) - LeetSync

--- daily-streak/67-add-binary/add-binary.java
+++ daily-streak/67-add-binary/add-binary.java
@@ -9,21 +9,11 @@ class Solution {
 
         while (iA >= 0 || iB >= 0 || carry==1) {
             int sum = carry;
-
             char cA = (iA >= 0) ? a.charAt(iA--) : '0';
             char cB = (iB >= 0) ? b.charAt(iB--) : '0';
-
             sum = sum + cA - 48 + cB - 48;
-
             carry = sum >= 2 ? 1 : 0;
-
-            if (sum == 1 || sum == 3) {
-                sum = 1;
-            } else {
-                sum = 0;
-            }
-
-            ansSb.append(sum);
+            ansSb.append(sum % 2);
         }
        return ansSb.reverse().toString();
     }
