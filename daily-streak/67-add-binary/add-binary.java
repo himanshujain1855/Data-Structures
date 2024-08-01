3c5b8d125310c426bd1b7bd755187904592d1f53
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-08-01 12:22:35 +0530

    Time: 2 ms (49.81%) | Memory: 42.1 MB (64.90%) - LeetSync

--- daily-streak/67-add-binary/add-binary.java
+++ daily-streak/67-add-binary/add-binary.java
@@ -3,41 +3,31 @@ class Solution {
         int iA = a.length() - 1;
         int iB = b.length() - 1;
 
-        StringBuilder ansSb=new StringBuilder();
+        StringBuilder ansSb = new StringBuilder();
 
-        char carry = '0';
-        
+        int carry = 0;
+
         while (iA >= 0 || iB >= 0) {
+            int sum = carry;
 
-            char cA = (iA >= 0) ? a.charAt(iA) : '0';
-            char cB = (iB >= 0) ? b.charAt(iB) : '0';
+            char cA = (iA >= 0) ? a.charAt(iA--) : '0';
+            char cB = (iB >= 0) ? b.charAt(iB--) : '0';
 
-            char[] temp = eval(cA + "" + cB + carry);
-            carry = temp[1];
-            ansSb.insert(0,temp[0]);
+            sum = sum + cA - 48 + cB - 48;
 
-            iA--;
-            iB--;
+            carry = sum >= 2 ? 1 : 0;
+
+            if (sum == 1 || sum == 3) {
+                sum = 1;
+            } else {
+                sum = 0;
+            }
+
+            ansSb.insert(0, sum);
         }
 
-        if (carry == '1') return "1" + ansSb;
+        if (carry == 1) return "1" + ansSb;
         return ansSb.toString();
-    }
-
-    static char[] eval(String s) {
-        char[] ans = new char[2];
-        ans[0] = '0';
-        ans[1] = '0';
-
-        if (s.equals("010") || s.equals("100") || s.equals("001") || s.equals("111")) {
-            ans[0] = '1';
-        }
-
-        if (s.equals("011") || s.equals("110") || s.equals("101") || s.equals("111")) {
-            ans[1] = '1';
-        }
-
-        return ans;
     }
 
 }
