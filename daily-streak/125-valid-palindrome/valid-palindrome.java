a6327be90535cd25b9879b2fb65320ebe3310e9c
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-08-05 11:06:54 +0530

    Time: 4 ms (55.81%) | Memory: 43.1 MB (62.78%) - LeetSync

--- daily-streak/125-valid-palindrome/valid-palindrome.java
+++ daily-streak/125-valid-palindrome/valid-palindrome.java
@@ -22,7 +22,7 @@ class Solution {
         int start=0;
         int end=word.length()-1;
 
-        while(start <=end){
+        while(start <end){
             if(word.charAt(start)!=word.charAt(end)) return false;
             start++;
             end--;
