7bd67d47eb2e69004fdfd71ea1caae6bc7aa193d
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-08-05 11:06:43 +0530

    Time: 1 ms (100.00%) | Memory: 45 MB (42.44%) - LeetSync

--- daily-streak/2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
+++ daily-streak/2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
@@ -7,11 +7,15 @@ class Solution {
         return "";
     }
 
-    public boolean isPalinDrome(String word){
-        int len=word.length();
+    public boolean isPalinDrome(String word) {
+        int start = 0;
+        int end = word.length() - 1;
 
-        for(int i=0; i<(len/2); i++){
-            if(word.charAt(i)!=word.charAt(len-i-1)) return false;
+        while (start < end) {
+            if (word.charAt(start) != word.charAt(end))
+                return false;
+            start++;
+            end--;
         }
         return true;
     }
