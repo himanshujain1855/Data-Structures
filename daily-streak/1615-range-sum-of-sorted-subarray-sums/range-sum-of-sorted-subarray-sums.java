2bdf25ceb7cd23e491617aa1f5fd268324c97079
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-08-05 13:13:31 +0530

    Time: 58 ms (87.71%) | Memory: 48.7 MB (38.52%) - LeetSync

--- daily-streak/1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
+++ daily-streak/1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -1,22 +1,24 @@
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
         int mod = 1000000007;
-        int cap = (int) (n * (((double) n + 1) / 2));
-        ArrayList<Integer> subArrList = new ArrayList<>(cap);
-
+        int cap = (int) (n * (((double)n + 1) / 2));
+        int[] arr = new int[cap];
+        int index=0;
         for (int i = 0; i < n; i++) {
             int sum = 0;
             for (int j = i; j < n; j++) {
                 sum += nums[j];
-                subArrList.add(sum);
+                arr[index]=sum;
+                index++;
             }
         }
-        if (!(cap == right && left == 1))
-            Collections.sort(subArrList);
+        if (!(cap == right && left == 1)){
+            Arrays.sort(arr);
+        }
         long ans = 0;
 
         for (int i = left; i <= right; i++) {
-            ans += (subArrList.get(i - 1) % mod);
+            ans += (arr[i - 1] % mod);
         }
         return (int) (ans % mod);
     }
