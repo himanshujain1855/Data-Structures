569821f89af0fa90468fb9c6e485717170bef4eb
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-08-06 17:23:32 +0530

    Time: 1 ms (33.63%) | Memory: 42.1 MB (9.88%) - LeetSync

---
+++ daily-streak/3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int len = word.length();
+
+        int[] chars = new int[26];
+        for (int i = 0; i < len; i++) {
+            chars[word.charAt(i) - 97]++;
+        }
+        Arrays.sort(chars);
+        int pushes = 0;
+
+        for (int i = 25; i >= 0; i--) {
+            int mul=4;
+            int mulDeciderIndex = 25 - i;
+
+            if (mulDeciderIndex <= 7) {
+                mul = 1;
+            } else if (mulDeciderIndex <= 15) {
+                mul = 2;
+            } else if (mulDeciderIndex <= 23) {
+                mul = 3;
+            }
+            pushes += (mul * chars[i]);
+        }
+
+        return pushes;
+    }
+}
