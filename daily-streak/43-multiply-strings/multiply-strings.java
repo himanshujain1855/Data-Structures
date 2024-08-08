9056742b2e14e68b1a3a177f91ec47f3acfc0665
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-08-08 10:01:21 +0530

    Time: 27 ms (5.00%) | Memory: 45.5 MB (5.90%) - LeetSync

---
+++ daily-streak/43-multiply-strings/multiply-strings.java
@@ -0,0 +1,106 @@
+class Solution {
+    public static String multiply(String num1, String num2) {
+        if (num1 == "0" || num2 == "0")
+            return "0";
+        if (num1 == "1")
+            return num2;
+        if (num2 == "1")
+            return num1;
+
+        int lenNum1 = num1.length();
+        int lenNum2 = num2.length();
+        StringBuilder ansSb = new StringBuilder("0");
+
+        String mul;
+        String mulT;
+
+        if (lenNum1 >= lenNum2) {
+            mul = num1;
+            mulT = num2;
+        } else {
+            mul = num2;
+            mulT = num1;
+        }
+
+        int lenMulT = mulT.length();
+
+        String temp;
+
+        for (int i = lenMulT - 1; i >= 0; i--) {
+            temp = multiply(mul, Integer.parseInt(mulT.charAt(i) + ""), lenMulT - i - 1);
+            ansSb = add(temp, ansSb.toString());
+        }
+
+        return ansSb.toString();
+    }
+
+    static String multiply(String mul, int digit, int zeros) {
+        if (digit == 0)
+            return "0";
+        if (digit == 1 && zeros==0)
+            return mul;
+        StringBuilder ansSb = new StringBuilder();
+        int lenMul = mul.length();
+
+        int car = 0;
+
+        for (int i = lenMul - 1; i >= 0; i--) {
+            int ans = Integer.parseInt(mul.charAt(i) + "") * digit + car;
+            if (ans > 9) {
+                car = ans / 10;
+                ansSb.append(ans % 10);
+            } else {
+                car = 0;
+                ansSb.append(ans);
+            }
+
+        }
+
+        if (car > 0) {
+            ansSb.append(car);
+        }
+
+        ansSb = ansSb.reverse();
+
+        for (int i = 0; i < zeros; i++) {
+            ansSb.append("0");
+        }
+
+        return ansSb.toString();
+    }
+
+    static StringBuilder add(String num1, String num2) {
+        if (Objects.equals(num1, "0"))
+            return new StringBuilder(num2);
+        if (Objects.equals(num2, "0"))
+            return new StringBuilder(num1);
+
+        int lenNum1 = num1.length() - 1;
+        int lenNum2 = num2.length() - 1;
+
+        StringBuilder ansSb = new StringBuilder();
+        int car = 0;
+
+        while (lenNum1 >= 0 || lenNum2 >= 0) {
+            int d1 = lenNum1 >= 0 ? Integer.parseInt(num1.charAt(lenNum1) + "") : 0;
+            int d2 = lenNum2 >= 0 ? Integer.parseInt(num2.charAt(lenNum2) + "") : 0;
+            int add = d1 + d2 + car;
+
+            if (add > 9) {
+                car = 1;
+                ansSb.append((add % 10));
+            } else {
+                car = 0;
+                ansSb.append(add);
+            }
+            lenNum2--;
+            lenNum1--;
+
+        }
+        if (car == 1) {
+            ansSb.append(1);
+        }
+
+        return ansSb.reverse();
+    }
+}
