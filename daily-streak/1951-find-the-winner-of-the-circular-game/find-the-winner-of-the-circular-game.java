5ed03f90ee14c47771facb9ed9c626e0d948b74c
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-08-15 18:23:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (94.38%) - LeetSync

--- daily-streak/1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ daily-streak/1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,14 +1,13 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        return findRealSurviver(n, k) + 1;
-    }
+        int currSurviverPos=0;
 
-    public int findRealSurviver(int n, int k) {
-        if (n == 1)
-            return 0;
-        int adjustedSurviver = findRealSurviver(n - 1, k);
-        int realSurviver = (adjustedSurviver + k) % n;
-        return realSurviver;
-    }
+        int i=1;
 
+        while(i<n){
+            currSurviverPos = (currSurviverPos + k) % (++i);
+        }
+
+        return currSurviverPos+1;
+    }
 }
