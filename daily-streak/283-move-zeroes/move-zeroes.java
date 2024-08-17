df3f4f5ccc93949379185e12f29c5fb94756ac6d
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-08-17 19:59:05 +0530

    Time: 2 ms (82.67%) | Memory: 46.1 MB (36.89%) - LeetSync

--- daily-streak/283-move-zeroes/move-zeroes.java
+++ daily-streak/283-move-zeroes/move-zeroes.java
@@ -1,14 +1,18 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int len=nums.length;
+        if(len==1) return;
         int count=0;
-        for(int i=0;i<len;i++){
-            if(nums[i]!=0){
-                int temp=nums[i];
-                nums[i]=0;
+        int start=0;
+
+        while(start<len){
+            if(nums[start]!=0){
+                int temp=nums[start];
+                nums[start]=0;
                 nums[count]=temp;
                 count++;
             }
+            start++;
         }
     }
 }
