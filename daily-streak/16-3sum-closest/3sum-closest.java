85402c3cfca5c52713a673270323383b0bbe4c1c
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-09-05 21:51:54 +0530

    Time: 17 ms (18.24%) | Memory: 43.8 MB (5.33%) - LeetSync

--- daily-streak/16-3sum-closest/3sum-closest.java
+++ daily-streak/16-3sum-closest/3sum-closest.java
@@ -1,20 +1,34 @@
 class Solution {
     public static int threeSumClosest(int[] nums, int target) {
         int len = nums.length;
+
+        Arrays.sort(nums);
+
         Integer currNearSum = null;
         for (int i = 0; i < len; i++) {
-            for (int j = i + 1; j < len; j++) {
-                for (int k = j + 1; k < len; k++) {
-                    int tempSum = nums[i] + nums[j] + nums[k];
-                    if (tempSum == target)
-                        return target;
-                    if (currNearSum != null) {
-                        if ((Math.abs(target - tempSum)) < Math.abs(target - currNearSum)) {
-                            currNearSum = tempSum;
-                        }
-                    } else {
+
+            int lo = i + 1;
+
+            int hi = len - 1;
+
+            while (lo < hi) {
+                int tempSum = nums[i] + nums[lo] + nums[hi];
+
+                if (tempSum == target)
+                    return target;
+
+                if (currNearSum != null) {
+                    if ((Math.abs(target - tempSum)) < Math.abs(target - currNearSum)) {
                         currNearSum = tempSum;
                     }
+                } else {
+                    currNearSum = tempSum;
+                }
+
+                if (tempSum > target) {
+                    hi--;
+                } else {
+                    lo++;
                 }
             }
         }
