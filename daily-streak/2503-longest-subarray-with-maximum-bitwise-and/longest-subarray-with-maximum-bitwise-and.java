ef45239ac05cd7ec02f6a9e06241b3902def36f2
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-09-14 20:30:23 +0530

    Time: 3 ms (89.89%) | Memory: 60.1 MB (21.91%) - LeetSync

---
+++ daily-streak/2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int ans = 0;
+
+        int n = nums.length;
+
+        if (n == 1)
+            return 1;
+
+        int maxAndValue = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > maxAndValue)
+                maxAndValue = nums[i];
+        }
+
+        int streak=0;
+        for (int i = 0; i < n; i++) {
+            if(nums[i]==maxAndValue){
+                streak++;
+                if(streak>ans) ans=streak;
+            }else{
+                streak=0;
+            }
+        }
+
+        return ans;
+    }
+}
