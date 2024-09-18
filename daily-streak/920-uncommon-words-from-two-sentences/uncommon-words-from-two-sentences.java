7fcc965e2f51fd564c7b3617c981c82799748169
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2024-09-18 23:34:26 +0530

    Time: 2 ms (100.00%) | Memory: 42.1 MB (34.64%) - LeetSync

--- daily-streak/920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.java
+++ daily-streak/920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.java
@@ -9,38 +9,34 @@ class Solution {
         HashMap<String, Integer> s2Map = new HashMap();
 
         for (String word : s1Words) {
-            if (s1Map.containsKey(word)) {
-                s1Map.put(word, s1Map.get(word) + 1);
-            } else {
-                s1Map.put(word, 1);
-            }
+            s1Map.put(word, s1Map.getOrDefault(word, 0) + 1);
         }
 
         for (String word : s2Words) {
-            if (s2Map.containsKey(word)) {
-                s2Map.put(word, s2Map.get(word) + 1);
-            } else {
-                s2Map.put(word, 1);
-            }
+            s2Map.put(word, s2Map.getOrDefault(word, 0) + 1);
         }
 
         for (Map.Entry<String, Integer> map : s1Map.entrySet()) {
-            if(map.getValue()==1 && !s2Map.containsKey(map.getKey())){
-                ans.add(map.getKey());
+            if (map.getValue() == 1) {
+                if (!s2Map.containsKey(map.getKey())) {
+                    ans.add(map.getKey());
+                }
             }
         }
 
         for (Map.Entry<String, Integer> map : s2Map.entrySet()) {
-            if(map.getValue()==1 && !s1Map.containsKey(map.getKey())){
-                ans.add(map.getKey());
+            if (map.getValue() == 1) {
+                if (!s1Map.containsKey(map.getKey())) {
+                    ans.add(map.getKey());
+                }
             }
         }
-        int size=ans.size();
-        
-        String[] finalAns=new String[size];
+        int size = ans.size();
 
-        for(int i=0;i<size;i++){
-            finalAns[i]=ans.get(i);
+        String[] finalAns = new String[size];
+
+        for (int i = 0; i < size; i++) {
+            finalAns[i] = ans.get(i);
         }
 
         return finalAns;
